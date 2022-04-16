dac3a858c3c3726d1dff2cffbcca9a671d7f344c
AimanZaharin <92364588+AimanZaharin@users.noreply.github.com>
2022-04-16 10:20:53 +0800

    Add files via upload

---
+++ UseBMIClass.java
@@ -0,0 +1,14 @@
+
+public class UseBMIClass {
+  public static void main(String[] args) {
+	  
+    BMI bmi1 = new BMI("John Doe", 18, 145, 70);
+    
+    System.out.println("The BMI for " + bmi1.getName() + " who is " + bmi1.getAge() + " years old is "
+      + bmi1.getBMI() + " " + bmi1.getStatus());
+    
+    BMI bmi2 = new BMI("Peter King", 215, 70);
+    System.out.println("The BMI for " + bmi2.getName() + " who is " + bmi2.getAge() + " years old is "
+      + bmi2.getBMI() + " " + bmi2.getStatus());
+  }
+}
