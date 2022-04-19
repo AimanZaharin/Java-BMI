addea363b402d0738377462652e93f7b9131c713
AimanZaharin <92364588+AimanZaharin@users.noreply.github.com>
2022-04-19 09:09:15 +0800

    Update UseBMIClass.java

--- UseBMIClass.java
+++ UseBMIClass.java
@@ -2,12 +2,12 @@
 public class UseBMIClass {
   public static void main(String[] args) {
 	  
-    BMI bmi1 = new BMI("John Doe", 18, 145, 70);
+    BMI bmi1 = new BMI("John Fab", 18, 145, 70);
     
     System.out.println("The BMI for " + bmi1.getName() + " who is " + bmi1.getAge() + " years old is "
       + bmi1.getBMI() + " " + bmi1.getStatus());
     
-    BMI bmi2 = new BMI("Peter King", 215, 70);
+    BMI bmi2 = new BMI("Joshua Peter", 215, 70);
     System.out.println("The BMI for " + bmi2.getName() + " who is " + bmi2.getAge() + " years old is "
       + bmi2.getBMI() + " " + bmi2.getStatus());
   }
